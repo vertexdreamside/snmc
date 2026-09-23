55cf1508c0b30dcfcce6bc36777a6b55147a2935
vertexdreamside <vertexdreamside@gmail.com>
2026-09-23 10:00:22 +0400

    Delete app/admin/(dashboard)/site directory

--- app/admin/(dashboard)/site/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getSite } from "@/lib/cms";
-import SiteEditor from "@/components/admin/SiteEditor";
-
-export default async function AdminSitePage() {
-  const site = await getSite();
-  return <SiteEditor initial={site} />;
-}
