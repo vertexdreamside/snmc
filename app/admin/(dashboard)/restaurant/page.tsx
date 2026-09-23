f263f2b7a82ea4e69ca7e2b6b06c3e26a12bd551
vertexdreamside <vertexdreamside@gmail.com>
2026-09-23 09:58:53 +0400

    Delete app/admin/(dashboard)/restaurant directory

--- app/admin/(dashboard)/restaurant/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getRestaurantPhotos } from "@/lib/cms";
-import RestaurantEditor from "@/components/admin/RestaurantEditor";
-
-export default async function AdminRestaurantPage() {
-  const photos = await getRestaurantPhotos();
-  return <RestaurantEditor initial={photos} />;
-}
