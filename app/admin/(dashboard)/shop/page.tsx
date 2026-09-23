f7b141d695947ae2d795585b6776ceb7bde79f19
vertexdreamside <vertexdreamside@gmail.com>
2026-09-23 10:00:13 +0400

    Delete app/admin/(dashboard)/shop directory

--- app/admin/(dashboard)/shop/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getShopCategories } from "@/lib/cms";
-import ShopEditor from "@/components/admin/ShopEditor";
-
-export default async function AdminShopPage() {
-  const categories = await getShopCategories();
-  return <ShopEditor initial={categories} />;
-}
