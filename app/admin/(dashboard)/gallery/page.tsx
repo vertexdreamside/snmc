674a26006bf8344ab219962db578def897e3eb5a
vertexdreamside <vertexdreamside@gmail.com>
2026-09-23 09:58:22 +0400

    Delete app/admin/(dashboard)/gallery directory

--- app/admin/(dashboard)/gallery/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getGalleryImages } from "@/lib/cms";
-import GalleryEditor from "@/components/admin/GalleryEditor";
-
-export default async function AdminGalleryPage() {
-  const images = await getGalleryImages();
-  return <GalleryEditor initial={images} />;
-}
