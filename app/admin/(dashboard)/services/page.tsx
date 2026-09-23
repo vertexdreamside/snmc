abcc9c861b671e44fae35b10fae6636f10aa01d4
vertexdreamside <vertexdreamside@gmail.com>
2026-09-23 09:59:45 +0400

    Delete app/admin/(dashboard)/services directory

--- app/admin/(dashboard)/services/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getServices } from "@/lib/cms";
-import ServicesEditor from "@/components/admin/ServicesEditor";
-
-export default async function AdminServicesPage() {
-  const services = await getServices();
-  return <ServicesEditor initial={services} />;
-}
