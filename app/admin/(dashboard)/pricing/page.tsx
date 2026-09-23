11f759255c1b0383b444f9994a0d00ab975ddbcb
vertexdreamside <vertexdreamside@gmail.com>
2026-09-23 09:58:29 +0400

    Delete app/admin/(dashboard)/pricing directory

--- app/admin/(dashboard)/pricing/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getPricing } from "@/lib/cms";
-import PricingEditor from "@/components/admin/PricingEditor";
-
-export default async function AdminPricingPage() {
-  const pricing = await getPricing();
-  return <PricingEditor initial={pricing} />;
-}
