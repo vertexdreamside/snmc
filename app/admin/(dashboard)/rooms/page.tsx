ff25bcd666c399d4dd50cb96ca69919d7d929abf
vertexdreamside <vertexdreamside@gmail.com>
2026-09-23 09:59:00 +0400

    Delete app/admin/(dashboard)/rooms directory

--- app/admin/(dashboard)/rooms/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getRooms } from "@/lib/cms";
-import RoomsEditor from "@/components/admin/RoomsEditor";
-
-export default async function AdminRoomsPage() {
-  const rooms = await getRooms();
-  return <RoomsEditor initial={rooms} />;
-}
