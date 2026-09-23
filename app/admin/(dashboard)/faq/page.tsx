2134afc8fe1ae0ef8339ff679220458c031e598b
vertexdreamside <vertexdreamside@gmail.com>
2026-09-23 09:58:14 +0400

    Delete app/admin/(dashboard)/faq directory

--- app/admin/(dashboard)/faq/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getFaq } from "@/lib/cms";
-import FaqEditor from "@/components/admin/FaqEditor";
-
-export default async function AdminFaqPage() {
-  const faq = await getFaq();
-  return <FaqEditor initial={faq} />;
-}
